a549b19aaafd9180c559b95222f46c47e64aa0c7
Haukur Mar Bodvarsson <haukurmar@gmail.com>
2023-07-27 14:41:50 +0200

    refactor: import refactoring

--- src/components/NextAppDirEmotionCacheProvider.tsx
+++ src/components/NextAppDirEmotionCacheProvider.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import * as React from "react";
+import { Fragment, JSX, ReactNode, useState } from "react";
 import type { EmotionCache, Options as OptionsOfCreateCache } from "@emotion/cache";
 import createCache from "@emotion/cache";
 import { useServerInsertedHTML } from "next/navigation";
@@ -10,15 +9,15 @@ export type NextAppDirEmotionCacheProviderProps = {
 	/** This is the options passed to createCache() from 'import createCache from "@emotion/cache"' */
 	options: Omit<OptionsOfCreateCache, "insertionPoint">;
 	/** By default <CacheProvider /> from 'import { CacheProvider } from "@emotion/react"' */
-	CacheProvider?: (props: { value: EmotionCache; children: React.ReactNode }) => React.JSX.Element | null;
-	children: React.ReactNode;
+	CacheProvider?: (props: { value: EmotionCache; children: ReactNode }) => JSX.Element | null;
+	children: ReactNode;
 };
 
 // Adapted from https://github.com/mui/material-ui/blob/master/examples/material-next-app-router-ts/src/components/ThemeRegistry/EmotionCache.tsx
 export const NextAppDirEmotionCacheProvider = (props: NextAppDirEmotionCacheProviderProps) => {
 	const { options, CacheProvider = DefaultCacheProvider, children } = props;
 
-	const [registry] = React.useState(() => {
+	const [registry] = useState(() => {
 		const cache = createCache(options);
 		cache.compat = true;
 		const prevInsert = cache.insert;
@@ -68,7 +67,7 @@ export const NextAppDirEmotionCacheProvider = (props: NextAppDirEmotionCacheProv
 		});
 
 		return (
-			<React.Fragment>
+			<Fragment>
 				{globals.map(({ name, style }) => (
 					<style
 						key={name}
@@ -84,7 +83,7 @@ export const NextAppDirEmotionCacheProvider = (props: NextAppDirEmotionCacheProv
 						dangerouslySetInnerHTML={{ __html: styles }}
 					/>
 				)}
-			</React.Fragment>
+			</Fragment>
 		);
 	});
 
